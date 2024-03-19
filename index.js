877d801fa442b0b578770bec6ccc4b8c2e586f0c
abel mengich <kipyegoabel2308@gmail.com>
2024-03-19 17:13:11 -0400

    test passed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const setofNumbers = [10, 5, 20,15]
+const max =  Math.max(...setofNumbers)
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:",mod);
+console.log("max:", max);
 
 // Write your code here
